5ea9ab90383e4f21746131b59b3372e131f1b2fa
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-20 12:57:58 -0300

    init commit

---
+++ src/main/java/br/com/votacao/VoteBem/VoteBemApplication.java
@@ -0,0 +1,13 @@
+package br.com.votacao.VoteBem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteBemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteBemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/votacao/VoteBem/VoteBemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.votacao.VoteBem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteBemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
