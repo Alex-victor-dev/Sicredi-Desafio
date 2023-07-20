310fe85ed2b2372673d35a7a8aac8de4d7394182
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-20 16:24:53 -0300

    implementa end point de teste

--- src/main/java/br/com/votacao/vote/bem/VoteBemApplication.java
+++ src/main/java/br/com/votacao/vote/bem/VoteBemApplication.java
@@ -2,10 +2,18 @@ package br.com.votacao.vote.bem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class VoteBemApplication {
-
+	@GetMapping
+	public String voteBem() {
+		return "Bem Vindo ao Vote Bem";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VoteBemApplication.class, args);
 	}
