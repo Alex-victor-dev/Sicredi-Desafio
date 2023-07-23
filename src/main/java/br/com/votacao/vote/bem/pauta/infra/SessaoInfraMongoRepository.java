70afe23a9b82c290f0213601406db2d35128ed1a
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-23 17:34:36 -0300

    remove classes e interfaces nÃao ultilizadas

--- src/main/java/br/com/votacao/vote/bem/pauta/infra/SessaoInfraMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.votacao.vote.bem.pauta.infra;
-
-import br.com.votacao.vote.bem.pauta.domain.SessaoVotacao;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface SessaoInfraMongoRepository extends MongoRepository <SessaoVotacao, UUID> {
-}
--- src/main/java/br/com/votacao/vote/bem/pauta/infra/VotoInfraMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.votacao.vote.bem.pauta.infra;
-
-import br.com.votacao.vote.bem.pauta.domain.Voto;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface VotoInfraMongoRepository extends MongoRepository<Voto, UUID> {
-}
