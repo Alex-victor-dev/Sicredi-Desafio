a48f9bed0606265d7162851730f8ed4806e62ee9
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-23 17:38:48 -0300

    deleta interface nao ultilizada de votoRepository

--- src/main/java/br/com/votacao/vote/bem/pauta/application/repository/VotoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.votacao.vote.bem.pauta.application.repository;
-
-import br.com.votacao.vote.bem.pauta.domain.Voto;
-
-public interface VotoRepository {
-    Voto registraVoto(Voto voto);
-}
