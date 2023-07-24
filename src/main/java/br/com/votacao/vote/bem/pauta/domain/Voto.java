e816e5873f0ab9ccc872c1000d8c6ea996239a35
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-24 13:01:15 -0300

    corrige classe de voto

--- src/main/java/br/com/votacao/vote/bem/pauta/domain/Voto.java
+++ src/main/java/br/com/votacao/vote/bem/pauta/domain/Voto.java
@@ -4,18 +4,22 @@ import br.com.votacao.vote.bem.pauta.application.api.voto.VotoRequest;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Voto {
+    @CPF
+    private String cpf;
     private OpcaoVoto opcaoVoto;
     private LocalDateTime momento;
 
     public Voto(VotoRequest request) {
         this.momento = LocalDateTime.now();
         this.opcaoVoto = request.getOpcaoVoto();
+        this.cpf = request.getCpf();
     }
 
 
