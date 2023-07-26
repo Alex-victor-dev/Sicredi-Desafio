21156e9902411aec1beca444289545e199d3b263
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-26 15:10:49 -0300

    add anotacao enablesqs

--- src/main/java/br/com/votacao/vote/bem/VoteBemApplication.java
+++ src/main/java/br/com/votacao/vote/bem/VoteBemApplication.java
@@ -2,6 +2,7 @@ package br.com.votacao.vote.bem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @SpringBootApplication
 @EnableScheduling
+@EnableSqs
 public class VoteBemApplication {
 	@GetMapping
 	public String voteBem() {
