c2890ca165769767c2d3a6b1037af53ffc955db4
Alex-Victor <alexvictoribeiro@gmail.com>
2023-07-27 14:13:06 -0300

    update create sqs and sns bat e sh

--- sh/create-sqs-and-sns--resultado-sessao.bat
+++ sh/create-sqs-and-sns--resultado-sessao.bat
@@ -1,5 +1,5 @@
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name resultado-sessao-queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name resultado-sessao-queue --profile localstack
 
-aws --endpoint-url=http://localhost:4566 sns create-topic --name resultado-sessao-topic
+aws --endpoint-url=http://localhost:4566 sns create-topic --name resultado-sessao-topic --profile localstack
 
-aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:resultado-sessao-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:resultado-sessao-queue --attributes RawMessageDelivery=true
+aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:resultado-sessao-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:resultado-sessao-queue --attributes RawMessageDelivery=true --profile localstack
